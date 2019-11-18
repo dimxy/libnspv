cded491812c8184e6000f4ac652ea2638f7308c2
dimxy <dimxy@komodoplatform.com>
2019-11-18 22:35:04 +0500

    so name fixed

--- build_kogs_and.sh
+++ build_kogs_and.sh
@@ -1,2 +1,2 @@
 
-gcc -shared ./src/Unity/cc/kogswrapper.c -o ../libnspv-bin/libkogswrapper.so -L ./.libs -Wl,-l:libnspv.so -I ./include/ -I ./src/logdb/include/ -fPIC
+gcc -shared ./src/Unity/cc/kogswrapper.c -o ../libnspv-bin/libkogsplugin.so -L ./.libs -Wl,-l:libnspv.so -I ./include/ -I ./src/logdb/include/ -fPIC
