e37c6ac07fd73261d7f454c0bef4b1a0684d23b9
dimxy <dimxy@komodoplatform.com>
2019-11-19 19:50:45 +0500

    set soname for secp & cc for unity

--- build_kogs_and.sh
+++ build_kogs_and.sh
@@ -1,7 +1,7 @@
 
-# gcc -shared ./src/Unity/cc/kogswrapper.c -o ../libnspv-bin/libkogsplugin.so -L ./.libs -Wl,-soname,libnspv.so -I ./include/ -I ./src/logdb/include/ -fPIC
+gcc -shared ./src/Unity/cc/kogswrapper.c -o ../libnspv-bin/libkogsplugin.so -L ./.libs -Wl,-soname,libnspv.so -I ./include/ -I ./src/logdb/include/ -fPIC
 # gcc -shared ./src/Unity/cc/kogswrapper.c -o ../libnspv-bin/libkogsplugin.so ./.libs/libnspv.so -I ./include/ -I ./src/logdb/include/ -fPIC
 # static link:
 # gcc -shared ./src/Unity/cc/kogswrapper.c -o ../libnspv-bin/libkogsplugin.so ./.libs/libnspv.a ./src/secp256k1/.libs/libsecp256k1.a -I ./include/ -I ./src/logdb/include/ -fPIC
 # gcc -shared ./src/Unity/cc/kogswrapper.c -o ../libnspv-bin/libkogsplugin.so ./.libs/libnspv.a -I ./include/ -I ./src/logdb/include/ -fPIC
-gcc -shared ./src/Unity/cc/kogswrapper.c -o ../libnspv-bin/libkogsplugin.so -L ./.libs -Wl,-soname,libnspv.so  -L ./src/secp256k1/.libs -Wl,-soname,libsecp256k1.so  -L ./src/tools/cryptoconditions/.libs -Wl,-soname,libcryptoconditions.so  -I ./include/ -I ./src/logdb/include/ -fPIC
+# gcc -shared ./src/Unity/cc/kogswrapper.c -o ../libnspv-bin/libkogsplugin.so -L ./.libs -Wl,-soname,libnspv.so  -L ./src/secp256k1/.libs -Wl,-soname,libsecp256k1.so  -L ./src/tools/cryptoconditions/.libs -Wl,-soname,libcryptoconditions.so  -I ./include/ -I ./src/logdb/include/ -fPIC
