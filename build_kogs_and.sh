4f770c3bc510d78a91bb54582fe5153e1aaecc23
dimxy <dimxy@komodoplatform.com>
2019-11-19 13:26:58 +0500

    direct link so

--- build_kogs_and.sh
+++ build_kogs_and.sh
@@ -1,2 +1,3 @@
 
-gcc -shared ./src/Unity/cc/kogswrapper.c -o ../libnspv-bin/libkogsplugin.so -L ./.libs -Wl,-l:libnspv.so -I ./include/ -I ./src/logdb/include/ -fPIC
+# gcc -shared ./src/Unity/cc/kogswrapper.c -o ../libnspv-bin/libkogsplugin.so -L ./.libs -Wl,-l:libnspv.so -I ./include/ -I ./src/logdb/include/ -fPIC
+gcc -shared ./src/Unity/cc/kogswrapper.c -o ../libnspv-bin/libkogsplugin.so ./.libs/libnspv.so -I ./include/ -I ./src/logdb/include/ -fPIC
