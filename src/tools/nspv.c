0b4c2b48d811a89f713dfa90e42556b7269b8f41
dimxy <dimxy@komodoplatform.com>
2019-12-02 16:56:08 +0500

    deb file mode fix

--- src/tools/nspv.c
+++ src/tools/nspv.c
@@ -98,7 +98,7 @@ void nspv_log_message(const char *format, ...)
     static FILE *fdebug = NULL;
     // TODO: make fdebug open multithreaded 
     if (fdebug == NULL) {
-        fdebug = fopen("nspv-debug.log", "w+");
+        fdebug = fopen("nspv-debug.log", "a");
     }
     if (fdebug)
         vfprintf(fdebug, format, args);
