eb27b75784dbcaf9e530ea71f26c115043018216
dimxy <dimxy@komodoplatform.com>
2020-05-28 03:40:38 +0500

    build kogswrapper sh macos

---
+++ build_kogs_mac.sh
@@ -0,0 +1,9 @@
+
+# gcc -shared ./src/tools/nspv.c -I ./src -I ./src/logdb/include -I ./include -DLIBNSPV_BUILD ./.libs/libbtc.a -o ../libnspv-bin/libnspv.so -O2  -Wno-unused-parameter -Wl,-L./src/secp256k1/.libs -Wl,-L./src/tools/cryptoconditions/.libs -Xlinker -levent -Wl,-soname,libcryptoconditions.so -Wl,-soname,libsecp256k1.so -Wl,-lsodium -Wl,-lpthread -fPIC
+
+gcc -shared ./src/Unity/cc/kogswrapper.c ./.libs/libnspv.a ./.libs/libbtc.a ./src/secp256k1/.libs/libsecp256k1.a ./src/tools/cryptoconditions/.libs/libcryptoconditions.a  -o ./.libs/libkogsplugin.dylib  -L ./.libs  -I ./include/  -I ./src/logdb/include/ -fPIC -levent -lgmp -lsodium
+# gcc -shared ./src/Unity/cc/kogswrapper.c -o ../libnspv-bin/libkogsplugin.so ./.libs/libnspv.so -I ./include/ -I ./src/logdb/include/ -fPIC
+# static link:
+# gcc -shared ./src/Unity/cc/kogswrapper.c -o ../libnspv-bin/libkogsplugin.so ./.libs/libnspv.a ./src/secp256k1/.libs/libsecp256k1.a -I ./include/ -I ./src/logdb/include/ -fPIC
+# gcc -shared ./src/Unity/cc/kogswrapper.c -o ../libnspv-bin/libkogsplugin.so ./.libs/libnspv.a -I ./include/ -I ./src/logdb/include/ -fPIC
+# gcc -shared ./src/Unity/cc/kogswrapper.c -o ../libnspv-bin/libkogsplugin.so -L ./.libs -Wl,-soname,libnspv.so  -L ./src/secp256k1/.libs -Wl,-soname,libsecp256k1.so  -L ./src/tools/cryptoconditions/.libs -Wl,-soname,libcryptoconditions.so  -I ./include/ -I ./src/logdb/include/ -fPIC
