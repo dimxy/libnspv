970d91b64a30bb6cdfc9dcef867617e64cb1b5ad
dimxy <dimxy@komodoplatform.com>
2020-05-27 23:48:26 +0500

    android def=no

--- androidonetime.sh
+++ androidonetime.sh
@@ -1,9 +1,17 @@
-./autogen.sh
-./configure
-cd src/tools/cryptoconditions
+#
+# make android libnspv in termux environment
+#
 ./autogen.sh
 ./configure --enable-androidso=yes
+
+# TODO: add cc lib to common Makefile.am
+echo making cryptoconditions lib...
+cd src/tools/cryptoconditions
+./autogen.sh
+./configure 
 make
+
+echo making libnspv and tools...
 cd ../../..
 make
 
